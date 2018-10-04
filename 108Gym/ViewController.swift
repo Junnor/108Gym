9691640ea61479a8f2066399474b293d1dd0505c
朱邓全 <zhudengquan@nyato.com>
2018-10-04 16:11:46 +0800

    日历点击事件的添加

--- 108Gym/ViewController.swift
+++ 108Gym/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     
     // Calendar
     var calendarView: CalendarView!
-    var textView: UITextView!
     
     // Ream
     let realm = try! Realm()
@@ -38,15 +37,6 @@ class ViewController: UIViewController {
         calendarView.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor, constant: 20).isActive = true
         calendarView.heightAnchor.constraint(equalTo: calendarView.widthAnchor).isActive = true
 
-        textView = UITextView()
-        textView.backgroundColor = UIColor.lightGray
-        view.addSubview(textView)
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: edge).isActive = true
-        textView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -edge).isActive = true
-        textView.topAnchor.constraint(equalTo: self.calendarView.bottomAnchor, constant: 10).isActive = true
-        textView.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.topAnchor, constant: -10).isActive = true
-
         CalendarView.Style.cellShape                = .bevel(8.0)
         CalendarView.Style.cellColorDefault         = UIColor.clear
         CalendarView.Style.cellColorToday           = UIColor(red:1.00, green:0.84, blue:0.64, alpha:1.00)
@@ -67,41 +57,28 @@ class ViewController: UIViewController {
         
         
         calendarView.backgroundColor = UIColor(red:0.31, green:0.44, blue:0.47, alpha:1.00)
-
-//        notification = results.observe({ (changes) in
-//            switch changes {
-//            case .initial:
-//                // Results are now populated and can be accessed without blocking the UI
-//                self.tableView.reloadData()
-//            case .update(_, let deletions, let insertions, let modifications):
-//                // Query results have changed, so apply them to the TableView
-//                self.tableView.beginUpdates()
-//                self.tableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-//                self.tableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-//                self.tableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-//                self.tableView.endUpdates()
-//            case .error(let err):
-//                // An error occurred while opening the Realm file on the background worker thread
-//                print("realm error = \(err)")
-//            }
-//        })
+        
+        let items = results.map {
+            return self.createCalendarEvent(check: $0)
+        }
+        print("items = \(items)")
+        self.calendarView.events = Array(items)
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         
         super.viewDidAppear(animated)
         
         let today = Date()
-        
-        var tomorrowComponents = DateComponents()
-        tomorrowComponents.day = 1
-        
-        
-        let tomorrow = self.calendarView.calendar.date(byAdding: tomorrowComponents, to: today)!
-        self.calendarView.selectDate(tomorrow)
-        
         self.calendarView.setDisplayDate(today)
+        
+        self.calendarView.reloadData()
     }
 
+    private func createCalendarEvent(check: GymCheckModel) -> CalendarEvent {
+        let event = CalendarEvent(title: "签到", startDate: Date.init(timeIntervalSince1970: check.date), endDate: Date.init(timeIntervalSince1970: check.date))
+        return event
+    }
     
     private func planStartDate() -> Date {
         let dateFormatter = DateFormatter()
@@ -135,7 +112,17 @@ extension ViewController: CalendarViewDelegate {
     }
     
     func calendar(_ calendar: CalendarView, didLongPressDate date: Date) {
+                
+        let alert = UIAlertController(title: "签到", message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "确定", style: .default) { (alert) in
+            self.addExerciseDate(date)
+        }
+        let cancel = UIAlertAction(title: "取消", style: .destructive, handler: nil)
+     
+        alert.addAction(ok)
+        alert.addAction(cancel)
         
+        self.present(alert, animated: true, completion: nil)
     }
     
     func calendar(_ calendar: CalendarView, didScrollToMonth date: Date) {
@@ -155,35 +142,17 @@ extension ViewController: CalendarViewDelegate {
 // MARK: - Add action
 extension ViewController {
     
-    @objc private func addAction() {
-        let alertVC = UIAlertController(title: nil, message: "打卡了", preferredStyle: .actionSheet)
-    
-        let exercise = UIAlertAction(title: "锻炼", style: .default) { (_) in
-            self.add(isExercise: true)
-        }
-        let check = UIAlertAction(title: "简单签到", style: .default) { (_) in
-            self.add(isExercise: false)
-        }
-        let cancel = UIAlertAction(title: "取消", style: .cancel, handler: nil)
-    
-        alertVC.addAction(exercise)
-
-        alertVC.addAction(check)
-        alertVC.addAction(cancel)
+    private func addExerciseDate(_ date: Date) {
         
-        present(alertVC, animated: true, completion: nil)
-    }
-    
-    private func add(isExercise: Bool) {
-
         let obj = GymCheckModel()
-        obj.date = Date().timeIntervalSince1970
-        obj.exercise = isExercise
-        obj.checked = !isExercise
-        
+        obj.date = date.timeIntervalSince1970
+
         try! realm.write {
             realm.add(obj)
         }
+        
+        let event = createCalendarEvent(check: obj)
+        self.calendarView.events.append(event)
         
     }
     
